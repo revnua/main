15176b620a06d1094de254ee137a3835c1bdee44
Vasyl Mosiychuk <161971314+revnua@users.noreply.github.com>
2024-03-03 00:56:45 +0200

    Change #1

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'jekyll', '4.2.2'
+
+group :jekyll_plugins do
+  gem 'jekyll-sitemap'
+  gem 'jekyll-paginate-v2', '3.0.0'
+  gem 'jekyll-get-json'
+end
---
+++ assets/js/main.ua.js
@@ -0,0 +1,12 @@
+---
+layout: null
+sitemap: false
+---
+
+{%- include js/jquery.min.js -%}
+{%- include js/popper.min.js -%}
+{%- include js/bootstrap.min.js -%}
+{%- include js/bootstrap.smoothscroll.min.js -%}
+{%- include js/tom-select.base.min.js -%}
+{%- include js/ua.home.js -%}
+{%- include js/realtyua.js -%}
